19a878ecc1928f813af1e017f463d8faf93db42b
LailatuKh <lailatulkhusna@std.unissula.ac.id>
2016-04-09 10:57:26 +0700

    Initial commit

---
+++ app/src/test/java/com/example/acer_id/iak_week1_lailatul/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.acer_id.iak_week1_lailatul;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
